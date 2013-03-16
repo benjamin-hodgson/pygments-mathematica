edf9746f91095a54bce29056e7bfc5e6539e1eb6
Benjamin Hodgson <poorsod@gmail.com>
2013-03-16 02:08:08 +0000

    Refactored lexer into groups

--- mathematica_lexer/mathematica_lexer.py
+++ mathematica_lexer/mathematica_lexer.py
@@ -1,4 +1,4 @@
-from pygments.lexer import RegexLexer, bygroups
+from pygments.lexer import RegexLexer, bygroups, include, using, this
 from pygments.token import *
 import re
 
@@ -6,27 +6,34 @@ class MathematicaLexer(RegexLexer):
     name = 'Mathematica'
     aliases = ['mathematica', 'Mathematica']
     filenames = ['*.m']
-    flags = re.DOTALL
     
     tokens = {
         'root': [
-            (r'\s+', Whitespace),
             (r'\(\*', Comment, 'comment'),  # comments (* look like this *)
-            (r'".*?"', String),
-            (r'[A-Z][A-Za-z0-9]*', Name.Builtin),  # builtins start with a capital letter
-            (r'[a-z][A-Za-z0-9]*', Name),  # user-defined names start with lowercase
+            (r'\s+', Whitespace),
+            (r'(?s)".*?"', String),
+            include('numbers'),
+            include('symbols'),
+            include('names')
+        ],
+        'numbers': [
             (r'[\+\-]?[0-9]+\.[0-9]*[eE]?[\+\-]?[0-9]*', Number.Float),
-            (r'[\+\-]?[0-9]+', Number.Integer),
+            (r'[\+\-]?[0-9]+', Number.Integer)
+        ],
+        'symbols': [
             (r'[\+\-/*=^:<>@\?~]', Operator),
             (r'[\[\]\(\){}]', Punctuation),  # various braces
-            (r'[,;\.]', Punctuation),
-            (r'_', Punctuation)
+            (r'[\.,;_]', Punctuation)
         ],
         'comment': [
             (r'[^\*\(\)]+', Comment),
             (r'\(\*', Comment, '#push'),
             (r'\*\)', Comment, '#pop'),
             (r'[\(\)\*]', Comment)  # star or brackets on their own
+        ],
+        'names': [
+            (r'[A-Z][A-Za-z0-9]*', Name.Builtin),  # builtins start with a capital letter
+            (r'[a-z][A-Za-z0-9]*', Name),  # user-defined names start with lowercase
         ]
     }
 
@@ -34,14 +41,14 @@ if __name__ == '__main__':
     from pygments import highlight
     from pygments.formatters import LatexFormatter
     test_code = """
-         normal code; (* a comment *)
-         (* comment (* (nested) *) *)
-         BuiltInFunctionCall[argument, {list,argument,-10.01e+12}];
-         assignment = {var, 2, "string"};
-         
-         (* multiline comment
-         ContainingAKeyword,
-         assignment = {1,3,"hello"},
-         aFunctionDefiniton[a_,b_] := a+b *)
-         """
+normal code; (* a comment *)
+(* comment (* (nested) *) *)
+BuiltInFunctionCall[argument, {list,argument,-10.01e+12}];
+assignment = {var, 2, "string"};
+
+(* multiline comment
+ContainingAKeyword,
+assignment = {1,3,"hello"},
+aFunctionDefiniton[a_,b_] := a+b *)
+"""
     print(highlight(test_code, MathematicaLexer(), LatexFormatter()))
