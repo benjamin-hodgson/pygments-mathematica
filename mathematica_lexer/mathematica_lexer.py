1fe87a3e611f3d5c74ccc986744e20beb3ee017d
Benjamin Hodgson <poorsod@gmail.com>
2013-03-17 18:46:38 +0000

    Failed attempt at highlighting arguments

--- mathematica_lexer/mathematica_lexer.py
+++ mathematica_lexer/mathematica_lexer.py
@@ -7,6 +7,39 @@ class MathematicaLexer(RegexLexer):
     aliases = ['mathematica', 'Mathematica']
     filenames = ['*.m']
     
+    def definition(lexer, match):
+        # This doesn't work! That's why it's not actually used anywhere.
+        
+        # r'^([a-z][A-Za-z0-9]*)(\[.*?\])(\s*)(:?=)(\s*)(.*?)$'
+        yield match.start(1), Name.Function, match.group(1)
+        for posn, tokentype, token in lexer.get_tokens_unprocessed(match.group()[match.end(1):]):
+            yield posn, tokentype, token
+        yield match.start(2), Punctuation, '['
+        
+        arguments = [arg + ',' for arg in match.group(2)[1:-1].split(',')][:-1]
+        posn = match.start(2) + 1
+        variable_list = []
+        for arg in arguments:
+            argmatch = re.match(r'(\s*)([A-Za-z0-9,_\?]+)(_\??)([A-Za-z0-9]*)(,?)', arg)
+            for tokentype, value in zip([Whitespace, Name.Variable,
+                                         Punctuation, None, Punctuation],
+                                        argmatch.groups()):
+                if value:
+                    if tokentype is None:  # deals with '_?NumericQ' and similar
+                        tokentype = Name.Builtin if value[1].isupper() else Name
+                    yield posn, tokentype, value
+                    posn += len(value)
+                if tokentype is Name.Variable: variable_list.append(value)
+        
+        yield match.end(2) - 1, Punctuation, ']'
+        if match.group(3): yield match.start(3), Whitespace, match.group(3)
+        yield match.start(4), Punctuation, match.group(4)
+        if match.group(5): yield match.start(5), Whitespace, match.group(5)
+        
+        for tup in lexer.get_tokens_unprocessed(match.group(6)):
+            yield tup
+        
+    
     tokens = {
         'root': [
             (r'\(\*', Comment, 'comment'),  # comments (* look like this *)
